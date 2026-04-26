c7557fb6f2c822ce82736ffee7bbbf7f17c7f914
Lucas Marins <lucasmarins2024@gmail.com>
2026-04-25 21:46:35 -0300

    feat (#106): otimizar tempo de resposta

--- profissionais/src/main/java/br/com/chronos/profissionais/MsProfissionaisApplication.java
+++ profissionais/src/main/java/br/com/chronos/profissionais/MsProfissionaisApplication.java
@@ -2,10 +2,8 @@ package br.com.chronos.profissionais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class MsProfissionaisApplication {
 
     public static void main(String[] args) {
